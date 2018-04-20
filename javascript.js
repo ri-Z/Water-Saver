6e99a61bd0b27448fcd0cade6bd5c223edb4c622
riZ <50037152@europeia.pt>
2018-04-20 17:00:35 +0100

    correcting conflicts

--- javascript.js
+++ javascript.js
@@ -42,6 +42,29 @@
 //
 //   getData();
 
+
+/////////////////////////////////////////////////////////////////////
+      // Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+
+/////////////////////////////////////////////////////////////////////
   var but = document.getElementById('submit');
 
   but.onclick = function send(){

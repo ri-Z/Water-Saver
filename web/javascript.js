084e9beeef54b529886b1f3ea1a496e615606543
riZ <50037152@europeia.pt>
2018-04-26 01:06:41 +0100

    added some pages from stefano
    index changed
    added userPage.html
    added post.html
    added browse.html
    added image.html
    added some images

--- web/javascript.js
+++ web/javascript.js
@@ -36,24 +36,24 @@ function show(data){
 
 /////////////////////////////////////////////////////////////////////
       // Menu-toggle button
-
-      $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
-      });
-
-      // Scrolling Effect
-
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
-
-            else {
-                  $('nav').removeClass('black');
-            }
-      })
+      // 
+      // $(document).ready(function() {
+      //       $(".menu-icon").on("click", function() {
+      //             $("nav ul").toggleClass("showing");
+      //       });
+      // });
+      //
+      // // Scrolling Effect
+      //
+      // $(window).on("scroll", function() {
+      //       if($(window).scrollTop()) {
+      //             $('nav').addClass('black');
+      //       }
+      //
+      //       else {
+      //             $('nav').removeClass('black');
+      //       }
+      // })
 
 /////////////////////////////////////////////////////////////////////
 

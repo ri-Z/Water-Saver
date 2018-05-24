02187dc213825f9d9d65ef477f6cddbf97370d73
riZ <50037152@europeia.pt>
2018-05-24 15:26:54 +0100

    changing and reparing all the project MAIN FUNCTIONS WORKING

--- server.js
+++ server.js
@@ -7,31 +7,51 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+//const ejs = require('ejs');
 const multer = require('multer');
+const path   = require('path');
+const serveStatic = require('serve-static');
+const bcrypt = require('bcrypt');
+  const saltRounds = 10;
 
-app.listen(3000, () => console.log('App running on port 3000!'));
+// app.set('view engine', 'ejs');
+// app.get('/', (req, res) => res.render('index'));
+
+const server = app.listen(3000, () => console.log('App running on port 3000!'));
+// app.use(serveStatic(path.join(__dirname, 'assets')));
+// app.use(serveStatic(path.join(__dirname, 'images')));
+// //HTML files are not cached, while everything else is for 1 day.
+// app.use(serveStatic(path.join(__dirname, 'web'), {
+//   maxAge: '1d',
+//   setHeaders: setCustomCacheControl
+// }));
+//
+// function setCustomCacheControl (res, path) {
+//   if (serveStatic.mime.lookup(path) === 'text/html') {
+//     // Custom Cache-Control for HTML files
+//     res.setHeader('Cache-Control', 'web, max-age=0')
+//   }
+// }
+app.use(express.static('images'));
+app.use(express.static('assets'));
 app.use(express.static('web'));
+
+// app.use(express.static(path.join(__dirname, 'web')));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
-//creating folder to store the uploaded files
-var upload = multer({ dest: 'uploads/' })
-
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-  // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
-});
 
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   port: "3306",
 	database: "WaterSaver"
+  //socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
 db.connect (function(err){
@@ -50,66 +70,106 @@ app.get('/CreateDB', function(req, res) {
  })
  });
 
-app.get('/user/:name/:admin/:age/:email/:password/', addUser);
-function addUser(request, response){
-  var reqData = request.params;
-  var admin = reqData.admin;
-  var name = reqData.name;
-  var age = reqData.age;
-  var email = reqData.email;
-  var password = reqData.password;
-
-  var space = ", ";
-  var nimda = "admin - ";
-  var ega = "age - ";
-  var liame = "email - ";
-  var drowssap = "password - ";
-  users[name] = nimda + admin + space + ega + age + space + liame + email + space + drowssap + password;
-
-  var data = JSON.stringify(users, null, 2);
-  fs.writeFile('usersData.json', data, finished);
-  function finished(err) {
-    console.log(users);
-    console.log("ALL SET");
-  }
-
-
-  response.send(data);
-	let sql = "INSERT INTO User (Admin, Name, Birth, Email, Password) VALUES ('"+admin+"', '"+name+"', '"+age+"', '"+email+"', '"+password+"')";
-	db.query(sql, function (err, result, fields) {
-		if (err) {
-			throw err;
-		}
-		console.log("User Inserted into Database");
-	});
-
-	// db.query("SELECT * FROM User", function (err, result, fields) {
-	// 	if (err) {
-	// 		throw err;
-	// 	}
-	// 	console.log("o select all é" + result);
-	// });
-}
+// app.get('/user/:name/:admin/:age/:email/:password/', addUser);
+// function addUser(request, response){
+//   var reqData = request.params;
+//   var admin = reqData.admin;
+//   var name = reqData.name;
+//   var age = reqData.age;
+//   var email = reqData.email;
+//   var password = reqData.password;
+//
+//   var space = ", ";
+//   var nimda = "admin - ";
+//   var ega = "age - ";
+//   var liame = "email - ";
+//   var drowssap = "password - ";
+//   users[name] = nimda + admin + space + ega + age + space + liame + email + space + drowssap + password;
+//
+//   var data = JSON.stringify(users, null, 2);
+//   fs.writeFile('usersData.json', data, finished);
+//   function finished(err) {
+//     console.log(users);
+//     console.log("ALL SET");
+//   }
+//
+//
+//   response.send(data);
+// 	let sql = "INSERT INTO User (Admin, Name, Birth, Email, Password) VALUES ('"+admin+"', '"+name+"', '"+age+"', '"+email+"', '"+password+"')";
+// 	db.query(sql, function (err, result, fields) {
+// 		if (err) {
+// 			throw err;
+// 		}
+// 		console.log("User Inserted into Database");
+// 	});
+// }
 
 app.get('/all', sendAll);
 function sendAll(request, response){
   response.send(users);
 }
 
-//"search bar like function STILL NOT IMPLEMENTED"
-app.get('/search/:name/', searchUser);
 
-function searchUser(request, response){
-  var name = request.params;
-  let sql = "SELECT ";
+//MUDAR ISTO PARA CREATEUSER E POR OS CAMPOS DO USER TBM
+//Insert data into Database with Post method
+app.post('/createUser', (request, response)=>{
+  //log da informacao qd escrevemos e clicamos no botao
+  console.log(request.body);
+  bcrypt.hash(request.body.password, saltRounds, function(err, hash) {
+    if(err) throw err;
+    let sql = 'INSERT INTO User(Admin, Name, Birth, Email, Password) VALUES (?, ?, ?, ?, ?);';
+    let values = [request.body.admin, request.body.name, request.body.age, request.body.email, hash];
 
+    db.query(sql, values, (err,result)=>{
+      if(err)throw err;
+      console.log(result);
+      console.log("User Inserted into Database WaterSaver");
+      response.send(result);
+    });
+  });
+});
 
-}
+//putting photos in local storage
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './web/uploads')
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
 
-// GET feed
-app.get('/feed', function(req, res){
-	db.query("SELECT * FROM User;", function (err, result, fields) {
-		if (err) throw err;
-		console.log(result);
-	});
+app.post('/uploadimage',multer({
+    storage: storage,
+    fileFilter: function(req, file, callback) {
+        var ext = path.extname(file.originalname)
+        if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg')
+                    {
+            return callback('Only images are allowed', null)
+        }
+        callback(null, true)
+    }
+}).single('file'), function(req, res) {
+  console.log(req.body);
+  console.log(req.file);
+  var data = [
+      req.body.title,
+      req.body.description,
+      req.body.floor,
+      req.body.room,
+      req.file.path
+    ];
+
+    var query = db.query("INSERT INTO Post(Title, Description, Floor, Room, Media) VALUES (?, ?, ?, ?, ?);" , data, function(err, rows) {
+      if (err) throw err;
+      res.redirect('/Profile.html');
+    });
+});
+
+app.get('/showPost', function(request, response) {
+  db.query("Select * From Post Order by idPost DESC;", function(err, result, fields) {
+    if (err) throw err;
+    //console.log(result);
+    response.send(result);
+  });
 });
--- web/javascript.js
+++ web/javascript.js
@@ -1,28 +1,4 @@
-// function person(name, age, address, mail, password, confPassword) {
-//   this.name = name;
-//   this.age = age;
-//   this.address = address;
-//   this.mail = mail;
-//   this.password = password;
-//   this.confPassword = confPassword;
-// }
-//
-// function createperson() {
-//
-//   if (document.getElementById('password').value === document.getElementById('confPassword').value) {
-//     name = document.getElementById('name').value;
-//     age = document.getElementById('age').value;
-//     address = document.getElementById('address').value;
-//     mail = document.getElementById('mail').value;
-//     password = document.getElementById('password').value;
-//     confPassword = document.getElementById('confPassword').value;
-//   } else {
-//     alert('passwords dont match');
-//   }
-//
-//   var obj = new person(name, age, address, mail, password, confPassword);
-//   console.log(obj);
-// }
+
 
 function getData(){
   $.getJSON('/all', show);
@@ -34,61 +10,71 @@ function show(data){
   getData();
 
 
-/////////////////////////////////////////////////////////////////////
-      // Menu-toggle button
-      // 
-      // $(document).ready(function() {
-      //       $(".menu-icon").on("click", function() {
-      //             $("nav ul").toggleClass("showing");
-      //       });
-      // });
-      //
-      // // Scrolling Effect
-      //
-      // $(window).on("scroll", function() {
-      //       if($(window).scrollTop()) {
-      //             $('nav').addClass('black');
-      //       }
-      //
-      //       else {
-      //             $('nav').removeClass('black');
-      //       }
-      // })
 
-/////////////////////////////////////////////////////////////////////
+//   var but = document.getElementById('submit');
+//
+//   but.onclick = function send(){
+//
+//     var admin = document.getElementsByName('admin').value;
+//     var name = document.getElementById('name').value;
+//     var age = document.getElementById('age').value;
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//
+//
+//     console.log(admin);
+//     console.log(name);
+//     console.log(age);
+//     console.log(email);
+//     console.log(password);
+//
+//     $.getJSON('/user/'+name+'/'+admin+'/'+age+'/'+email+'/'+password, end);
+//
+// function end(data){
+//   console.log(data);
+// }
+// }
 
-  var but = document.getElementById('submit');
-
-  but.onclick = function send(){
-
-    var admin = document.getElementById('admin').value;
-    // if (document.getElementById('admin').checked == 0) {
-    //   var admin = "0";
-    // } else {
-    //   var admin = "1";
-    // }
-    var name = document.getElementById('name').value;
-    var age = document.getElementById('age').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-
-    console.log(admin);
-    console.log(name);
-    console.log(age);
-    console.log(email);
-    console.log(password);
-
-    $.getJSON('/user/'+name+'/'+admin+'/'+age+'/'+email+'/'+password, end);
-
-function end(data){
-  console.log(data);
+function PwValidate() {
+  if (document.getElementById('password').value != document.getElementById('confPassword').value) {
+    AlertPwDontMatch.innerHTML = "Passwords don't match!";
+  } else {
+    AlertPwDontMatch.innerHTML = "";
+  }
 }
 
-// similar behavior as an HTTP redirect
-//window.location.replace("feedPage.html");
+function showPost() {
 
-// similar behavior as clicking on a link
-// window.location.href = "feedPage.html";
-// document.getElementById('data').innerHTML = data;
+  //DISPLAY THE POSTED PICTURE WITH INFO
+  $.getJSON('/showPost', success); //GET THE RESULT FROM THIS ROUTE AND EXECUTES THE FUNCTION 'SUCCESS'
+
+  function success(data) {
+    for (var i = 0; i < data.length; i++) {
+
+      var title = document.createElement('h5');
+      var img = document.createElement('img');
+      var column = document.createElement('div');
+
+      console.log(data);
+
+      title.innerHTML = data[i].Title;
+      var stringImg = data[i].Media.split('web/').pop();
+
+      // title.setAttribute('position', 'absolute');
+      title.setAttribute('top', '5px');
+      title.setAttribute('left', '5px');
+
+      img.setAttribute('src', stringImg);
+      img.setAttribute('width', "400px");
+      img.setAttribute('height', "400px");
+      // img.setAttribute('position', "relative");
+
+      column.setAttribute('class', 'column');
+
+      var div = document.getElementById('row');
+      column.appendChild(title);
+      column.appendChild(img);
+      div.appendChild(column);
+    }
+  }
 }

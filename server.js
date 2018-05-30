30ad58d83c0492e4df8c439112be22e02890117d
Stefano <34067533+StefanoClaro@users.noreply.github.com>
2018-05-30 16:13:07 +0100

    CHANGES

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ passport.deserializeUser(function(user_id, done) {
 const options = {
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "Prune098",
   port: "3306",
   database: "WaterSaver"
 };
@@ -84,7 +84,7 @@ var sessionStore = new MySQLStore(options);
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "Prune098",
   port: "3306",
 	database: "WaterSaver"
   //socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
